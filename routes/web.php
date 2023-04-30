326d3ed0a515f81db7a042906b775ec6d347db16
kawamoto06 <kawamoto0605@gmail.com>
2023-04-30 14:39:06 +0900

    aa

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,4 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index']);
 
-Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index']);
