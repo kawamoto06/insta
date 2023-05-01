da27cd2eac789ad8572e8bd78db1138e3a01d1d0
kawamoto06 <kawamoto0605@gmail.com>
2023-05-01 10:44:41 +0900

    css

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,24 +1,25 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="UTF-8">
-  <link rel="stylesheet" href="{{ asset('example.css') }}">
-  <title>サンプルのサイト</title>
-</head>
-<body>
-<!-- header -->
-<header>
-  <h1 class="headline">
-    <a>サンプルのサイト</a>
-  </h1>
-  <ul class="nav-list">
-    <li class="nav-list-item">
-      <a>Home</a>
-    </li>
-    <li class="nav-list-item">About</li>
-    <li class="nav-list-item">Topic</li>
-  </ul>
-</header>
+@include('layouts.header')
+<ul class="nav-list">
+    
+  <li class="nav-list-item">
+      @if(Auth::check())
+      <!-- ログアウトボタンを表示 -->
+      <form method="POST" action="{{ route('logout') }}">
+          @csrf
+          <a href="#" onclick="event.preventDefault(); this.closest('form').submit();">ログアウト</a>
+      </form>
+      @else
+      <a href="{{ route('login') }}">ログイン</a>
+  @endif</li>
+  <li class="nav-list-item">
+      @if(Auth::check())
+      <a href="{{ route('post.create') }}">投稿する</a>
+      @else
+      <a href="{{ route('register') }}">新規登録</a>
+      @endif
+  </li>
+</ul>
+
 <form action="{{ route('post.store') }}" method="POST">
     @csrf
     <div>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,41 +1,28 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="UTF-8">
-  <link rel="stylesheet" href="{{ asset('example.css') }}">
-  <title>サンプルのサイト</title>
-</head>
-<body>
-<!-- header -->
-<header>
-  <h1 class="headline">
-    <a>サンプルのサイト</a>
-  </h1>
-  <ul class="nav-list">
+@include('layouts.header')
+<ul class="nav-list">
     
-    <li class="nav-list-item">
-        @if(Auth::check())
-        <!-- ログアウトボタンを表示 -->
-        <form method="POST" action="{{ route('logout') }}">
-            @csrf
-            <a href="#" onclick="event.preventDefault(); this.closest('form').submit();">ログアウト</a>
-        </form>
-        @else
-        <a href="{{ route('login') }}">ログイン</a>
-    @endif</li>
-    <li class="nav-list-item">
-        @if(Auth::check())
-        <a href="{{ route('post.create') }}">投稿する</a>
-        @else
-        <a href="{{ route('register') }}">新規登録</a>
-        @endif
-    </li>
-  </ul>
-</header>
+  <li class="nav-list-item">
+      @if(Auth::check())
+      <!-- ログアウトボタンを表示 -->
+      <form method="POST" action="{{ route('logout') }}">
+          @csrf
+          <a href="#" onclick="event.preventDefault(); this.closest('form').submit();">ログアウト</a>
+      </form>
+      @else
+      <a href="{{ route('login') }}">ログイン</a>
+  @endif</li>
+  <li class="nav-list-item">
+      @if(Auth::check())
+      <a href="{{ route('post.create') }}">投稿する</a>
+      @else
+      <a href="{{ route('register') }}">新規登録</a>
+      @endif
+  </li>
+</ul>
 @if ($user)
-    <p>こんにちは、{{ $user->name }}さん！</p>
+    <p class="hello">こんにちは、{{ $user->name }}さん！</p>
 @else
-    <p>ログインしていません。</p>
+    <p class="hello">ログインしていません。</p>
 @endif
 
 @section('title', 'Board.index')
