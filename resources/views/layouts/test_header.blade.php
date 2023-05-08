c5e930d091da883870bd967c302313900a4bc2e3
kawamoto06 <kawamoto0605@gmail.com>
2023-05-08 16:41:21 +0900

    キメラ

--- resources/views/layouts/test_header.blade.php
+++ resources/views/layouts/test_header.blade.php
@@ -1,4 +1,16 @@
-<header>
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+	  <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@10/build/styles/github.min.css">
+    <link rel="stylesheet" href="{{ asset('test.css') }}" >
+     <link rel="stylesheet" href="{{ asset('example.css') }}">
+    <title>ポートフォリオ</title>
+  </head>
+  <body>
+  <header>
   <nav class="navbar navbar-dark bg-dark">
     <!-- タイトル -->
     <a class="navbar-brand" href="/">ポートフォリオ</a>
--- resources/views/posts/test.blade.php
+++ resources/views/posts/test.blade.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="ja">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-	  <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@10/build/styles/github.min.css">
-    <link rel="stylesheet" href="{{ asset('test.css') }}" >
-    <title>ポートフォリオ</title>
-  </head>
-  <body>
+
 
 @include('layouts.test_header')
 
